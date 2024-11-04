515a0d55d550e832a542e0a35dbea6fc570234d7
EnricoGenovese <enricogeno@libero.it>
2024-11-04 16:44:59 +0100

    init jsnack-6

---
+++ jsnack-6/script/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+console.clear();
+
+// 1 - Stampa il cubo dei primi N numeri
+// 2 - N è un numero indicato dall'utente
+
+// Dato un numero n inserito dall'utente, un ciclo 'for' con interazioni massimo uguali (<=) al
+// valore dell'intero indicato dall'utente, ed elevato al cubo per ogni iterazione, potrebbe risolvere
+// la consegna.
