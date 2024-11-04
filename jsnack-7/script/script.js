95607922a33dd6f9f66f09ce31ed6742cf21598f
EnricoGenovese <enricogeno@libero.it>
2024-11-04 17:30:23 +0100

    jsnack-7 init

---
+++ jsnack-7/script/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+console.clear();
+
+// 1 - Stampa le potenze del 2 fino a 1000
+
+/*
+Immagino che 1000 sia il valore massimo che il risultato della potenza debba raggiungere.
+    (2^1000 = 1.071509e+301, sono tanti 0)
+*/
+
