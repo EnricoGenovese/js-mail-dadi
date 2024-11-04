5db38f36e13d67e8b73151b22f02fa21b77fa266
EnricoGenovese <enricogeno@libero.it>
2024-11-04 16:31:10 +0100

    add 'while loop'; add commentary for future reference

--- jsnack-5/script/script.js
+++ jsnack-5/script/script.js
@@ -8,3 +8,23 @@ console.clear();
     se questo è dispari, inseriscilo nell'array.
 */
 
+// Creiamo un array vuoto, in cui verranno inseriti solamente i numeri dispari digitati dall'utente.
+
+const oddNumbs = [];
+
+// Il risultato può essere raggiunto utilizzando un ciclo 'while', permettendo soltanto 6 possibilita 
+// di input utilizzando un contatore che raggiunto il valore 6 esce dal loop (e stampa l'array creato);
+
+let c = 0; // il contatore
+
+while(c < 6) { 
+    const userNumb = prompt("Inserisci un numero"); // richiesta all'utente di inserire un numero
+    // Per ogni iterazione il contatore sale di 1
+    c++;
+    // Aggiunta del numero all'array solo se dispari
+    if(parseInt(userNumb) % 2 !== 0) {
+        oddNumbs.push(userNumb); // aggiunge in coda all'array il numero che passa la condizione
+    }  
+} 
+
+console.log(oddNumbs); // stampa l'array dopo le 6 iterazioni
