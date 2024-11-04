1048e0b822c61b1debe07f71c57d614add6a56bc
EnricoGenovese <enricogeno@libero.it>
2024-11-04 14:26:45 +0100

    set mail validation variables

--- script/script.js
+++ script/script.js
@@ -9,6 +9,20 @@ stampa un messaggio appropriato sull’esito del controllo.
 Non è necessario provvedere alla validazione delle email
 */
 
+// Viene creato un array contenente gli indirizzi e-mail che si vogliono controllare
+const mailAddress = [
+    "pippo@gmail.com",
+    "pluto@gmail.com",
+    "topolino@gmail.com",
+    "paperino@gmail.com",
+    "quiquoqua@gmail.com"
+]
+
+// si chiede all'utente di inserire il proprio indirizzo e-mail
+const userEMail = prompt("Inserisci il tuo indirizzo e-mail")
+
+// si imposta una variabile 'flag' per permettere al loop di interrompesi quando si trova una corrispondenza
+let welcome = false;
 
 
 
