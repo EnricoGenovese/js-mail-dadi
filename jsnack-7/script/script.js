0df8a465471e08889bf9cca9e299a9a86d60c458
EnricoGenovese <enricogeno@libero.it>
2024-11-04 18:15:30 +0100

    set variables; add comments for future reference

--- jsnack-7/script/script.js
+++ jsnack-7/script/script.js
@@ -9,3 +9,13 @@ Immagino che 1000 sia il valore massimo che il risultato della potenza debba rag
     (2^1000 = 1.071509e+301, sono tanti 0)
 */
 
+// Un 'while loop' potrebbe servire ad interrompere il codice una volta raggiunto il valore desiderato.
+// La condizione del ciclo sarebbe: risultato <= 1000.
+
+const maxValue = 1000;  // valore oltre il quale il ciclo non itera più
+let result = 0;         // la variabile di immagazzinamento dell'operazione nel loop
+let i = 1;              // il valore dell'elevatore a potenza di 2
+
+
+
+
